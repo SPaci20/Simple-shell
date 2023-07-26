14edfccb218869ca6027c136a908336f82451b3e
SPaci20 <spaci20@gmail.com>
2023-07-26 15:18:59 -0700

    Handler update

--- builtin_handler.c
+++ builtin_handler.c
@@ -10,7 +10,7 @@
 int handle_builtin(char **command, char *line)
 {
 	/* Check if the command is a builtin */
-	struct builtin builtin = {"env", "exit", "setenv", "unsetenv"};
+	struct builtin builtin = {"env", "exit"};
 
 	if (strcmp(*command, builtin.env) == 0)
 	{
@@ -20,46 +20,23 @@ int handle_builtin(char **command, char *line)
 	}
 	else if (strcmp(*command, builtin.exit) == 0)
 	{
-		/* ... (existing implementation) ... */
-	}
-	else if (strcmp(*command, builtin.setenv) == 0)
-	{
-		/* Set environment variable */
-		if (command[1] == NULL || command[2] == NULL)
+		/* Check if "exit" command has an argument */
+		if (command[1] != NULL)
 		{
-			/* Invalid syntax, print an error message */
-			fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
-			return (1);
+			/* Convert the argument to an integer */
+			int exit_status = atoi(command[1]);
+			/* Free the memory */
+			free(line);
+			free_buffers(command);
+			/* Exit the shell with the specified status code */
+			exit(exit_status);
 		}
 		else
 		{
-			/* Call the setenv function to set the environment variable */
-			if (setenv(command[1], command[2], 1) != 0)
-			{
-				/* Print an error message on failure */
-				fprintf(stderr, "Error setting environment variable.\n");
-			}
-			return (1);
-		}
-	}
-	else if (strcmp(*command, builtin.unsetenv) == 0)
-	{
-		/* Unset environment variable */
-		if (command[1] == NULL)
-		{
-			/* Invalid syntax, print an error message */
-			fprintf(stderr, "Usage: unsetenv VARIABLE\n");
-			return (1);
-		}
-		else
-		{
-			/* Call the unsetenv function to remove the environment variable */
-			if (unsetenv(command[1]) != 0)
-			{
-				/* Print an error message on failure */
-				fprintf(stderr, "Error unsetting environment variable.\n");
-			}
-			return (1);
+			/* Exit the shell with the default status code 0 */
+			free(line);
+			free_buffers(command);
+			exit(0);
 		}
 	}
 	/* The command is not a builtin */
