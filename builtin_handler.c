1ab8ba1f973f83fa95c2de66d11bffdfdf5f1845
SPaci20 <spaci20@gmail.com>
2023-07-26 15:02:02 -0700

    Handler update

--- builtin_handler.c
+++ builtin_handler.c
@@ -17,13 +17,28 @@ int handle_builtin(char **command, char *line)
 		/* Print the environment */
 		print_env();
 		return (1);
-	} else if (strcmp(*command, builtin.exit) == 0)
+	}
+	else if (strcmp(*command, builtin.exit) == 0)
 	{
-		/* Exit the shell */
-		exit_cmd(command, line);
-		return (1);
+		/* Check if "exit" command has an argument */
+		if (command[1] != NULL)
+		{
+			/* Convert the argument to an integer */
+			int exit_status = atoi(command[1]);
+			/* Free the memory */
+			free(line);
+			free_buffers(command);
+			/* Exit the shell with the specified status code */
+			exit(exit_status);
 		}
+		else
+		{
+			/* Exit the shell with the default status code 0 */
+			free(line);
+			free_buffers(command);
+			exit(0);
+		}
+	}
 	/* The command is not a builtin */
 	return (0);
 }
-
